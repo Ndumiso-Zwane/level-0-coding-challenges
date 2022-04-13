9a845161df4f7423d6ffc51ca7ee67ea3204c606
Ndumiso Zwane <zwanenmm@gmail.com>
2022-04-13 13:21:24 +0200

    Completed task 7

---
+++ task-07.js
@@ -0,0 +1,13 @@
+function celsiusToFahrenheit(cels){
+    return (cels * 9) / 5 + 32;
+}
+
+function fahrenheitToCelsius(fahr){
+   return ((fahr - 32) * 5) / 9;
+}
+
+;
+console.log(celsiusToFahrenheit(98));
+
+
+console.log(fahrenheitToCelsius(98));
