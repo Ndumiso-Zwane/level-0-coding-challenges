2f231648aadd3b6efbf9e37e5094b77c1c148381
Ndumiso Zwane <zwanenmm@gmail.com>
2022-04-21 11:47:29 +0200

    Added additional changes

--- task-10.js
+++ task-10.js
@@ -1,11 +1,11 @@
 function commonLetters(sentence1, sentence2){
     const common = [];
     for (let x = 0; x < sentence1.length; x++){
-        if (sentence2.toLowerCase().includes(sentence1[x])){
-            common.push(sentence1[x]);
+        if (sentence2.toLowerCase().includes(sentence1[x].toLowerCase())){
+            common.push(sentence1[x].toLowerCase());
         }
     }
     return "Common letters: " + common;
 }
 
-console.log(commonLetters("house", "compUters"));
+console.log(commonLetters("houSe", "compUters"));
